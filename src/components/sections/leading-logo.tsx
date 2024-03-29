ca691471b434108fd3716d15bb47ab57d251cf1e
dereineolli <olli@dereineolli.de>
2024-03-29 23:05:25 +0100

    fix: import svg

--- src/components/sections/leading-logo.tsx
+++ src/components/sections/leading-logo.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { SlideIn } from "./slide-in";
 
+import logo from "../../images/logo.svg";
+
 export const LeadingLogo = () => (
     <>
         <div className="flex flex-col items-center justify-center h-[80vh] max-w-full">
             <SlideIn className={"flex flex-row items-center justify-center text-center px-8 gap-9"}>
-                <img className="object-cover w-36 h-36" src={"../../logo.svg"} alt="Beziehungswegen" />
+                <img className="object-cover w-36 h-36" src={logo} alt="Beziehungswegen" />
                 <h1 className="font-autography text-4xl sm:text-7xl lg:text-9xl text-primary-700 leading">
                     beziehungswegen
                 </h1>
---
+++ src/utils.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, import/no-default-export */
+declare module "*.svg" {
+    const content: any;
+    export default content;
+}
