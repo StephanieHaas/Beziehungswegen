1432d858335fdaf610c7121eff25084e66a220e9
dereineolli <olli@dereineolli.de>
2024-03-29 22:05:11 +0100

    feat: add sections component

--- src/components/sections/slide-in.tsx
+++ src/components/sections/slide-in.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 
-export const SlideIn: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-    <div className="flex flex-col items-center justify-center h-3/4 animate-slideIn">
-        <div className="flex flex-row items-center justify-center text-center px-8 gap-9">{children}</div>
-    </div>
-);
+export const SlideIn: React.FC<{
+    children: React.ReactNode;
+    className?: React.HTMLAttributes<HTMLDivElement>["className"];
+}> = ({ children, className }) => <div className={`animate-slideIn ${className}`}>{children}</div>;
