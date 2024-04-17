a79eb68f3a106c8cccc8bb344c86d41690e5da4f
dereineolli <olli@dereineolli.de>
2024-04-17 23:12:18 +0200

    feat: decrease section height

--- src/components/sections/leading-logo.tsx
+++ src/components/sections/leading-logo.tsx
@@ -5,7 +5,7 @@ import logo from "../../images/logo.svg";
 
 export const LeadingLogo = () => (
     <>
-        <div className="flex flex-col items-center justify-center h-[80vh] max-w-full">
+        <div className="flex flex-col items-center justify-center h-[60vh] max-w-full">
             <SlideIn className={"flex flex-row items-center justify-center text-center px-8 gap-9"}>
                 <img className="object-cover w-36 h-36" src={logo} alt="Beziehungswegen" />
                 <h1 className="font-autography text-4xl sm:text-7xl lg:text-9xl text-primary-700 leading">
