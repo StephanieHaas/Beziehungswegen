9f36a0c46572b190b40d323c2526f475b833ddd7
dereineolli <olli@dereineolli.de>
2024-04-21 11:55:55 +0200

    fix: use clear for image floating

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Footer = () => (
     <>
-        <footer className="flex flex-col items-center justify-center py-4">
+        <footer className="flex flex-col items-center justify-center py-4 clear-both">
             <Link to="/impressum">Impressum</Link>
         </footer>
     </>
